ff65dee3d78848cefecf3dc578cf3d01fa73704a
michael-szczesny <mszczesn@uos.de>
2023-01-25 23:32:01 +0000

    read file

--- index.php
+++ index.php
@@ -17,3 +17,17 @@ echo "$t->age, $t->name".PHP_EOL;
 $arr[] = $t;
 
 echo json_encode($arr);
+
+$s = "1, 2, 3, 4, 5, 6";
+
+$res = explode(', ', $s);
+
+var_dump(array_map('intval', $res));
+
+$file = fopen('data.csv', 'r');
+$arr = [];
+while ($data = fgetcsv($file, null, ',')) {
+    $arr[] = array_map('intval', $data);
+}
+fclose($file);
+var_dump($arr);
